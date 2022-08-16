164fd011c54a4d69e79e4a80b434cb8a58fd4801
anurup69 <95045625+anurup69@users.noreply.github.com>
2022-08-16 12:46:20 +0530

    Add files via upload

---
+++ Admin.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Admin.css'
+
+
+
+
+const Admin =() => {
+  return (
+    <>
+        
+
+<div className="parent_box">
+	<div className="box-form">
+		<div class="left">
+			<div className="overlay">
+			<h1>Hello Admin!!</h1>
+			 {/* <button>Login</button>  */}
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Curabitur et est sed felis aliquet sollicitudin</p>
+			<span>
+				 {/* <p>login with social media</p> */}
+				 {/* <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a> */}
+				<a href="#"><i className="log" aria-hidden="true"></i> Admin Login</a>
+				{/* <button>Login</button>  */}
+			</span>
+			</div>
+		</div>
+		
+		
+	</div>
+</div>
+
+
+  
+
+
+
+    </>
+  )
+}
+
+export default Admin
