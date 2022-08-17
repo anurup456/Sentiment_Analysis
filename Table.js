bd359d5a7a6a4e155623c9f93fa549b042682011
anurup69 <95045625+anurup69@users.noreply.github.com>
2022-08-17 16:08:49 +0530

    Add files via upload

--- Table.js
+++ Table.js
@@ -2,25 +2,58 @@ import React,{useState} from 'react'
 import { Box, flexbox } from '@mui/system'
 import { Modal, Typography } from '@mui/material'
 import {Button} from '@mui/material'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 // import  Typography  from '@mui/material/styles/createTypography'
-import Popup from  '/home/anurup/Desktop/todo/src/Components/popup.js';
+// import Popup from  '/home/anurup/Desktop/todo/src/Components/popup.js';
 
 
 const Table =() => {
   const [open, setOpen]= useState(false)
-  const[openPopup, setOpenPopup]=useState(false)
+  const overlayStyle = { background: 'rgba(0,0,0,0.5)' };
+
+  function toggleModal(id){
+    const myModal = document.getElementById(id);
+    if(myModal.classList.contains("show")){
+      myModal.classList.remove("show");
+      myModal.style.display = "none"
+;    }else{
+      myModal.classList.add("show");
+      myModal.style.display="block"
+    }
+  }
+  // const[openPopup, setOpenPopup]=useState(false)
   return (
     <>
 
-  <div class="content">
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary"  onClick={()=> toggleModal("exampleModal")}>Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div className="content">
     
-    <div class="container">
+    <div className="container">
       <h2 class="mb-5">Table #6</h2>
       
 
-      <div class="table-responsive">
+      <div className="table-responsive">
 
-        <table class="table table-striped custom-table">
+        <table className="table table-striped custom-table">
           <thead>
             <tr>
               
@@ -40,8 +73,8 @@ const Table =() => {
                       </td>
                       <td><a href="#">Atomic bomb</a></td>
                       <td>
-                        <Button onClick={()=> setOpen(true)}>Open Dialogue </Button>
-                        {/* <Modal open={open} onClose={()=> setOpen(false)} >
+                        <Button onClick={()=> toggleModal("exampleModal")}>Open Dialogue </Button>
+                        <Modal open={open} onClose={()=> setOpen(false)} >
                           
                             <Box >
                               <Typography>
@@ -50,11 +83,15 @@ const Table =() => {
                             </Box>
 
 
-                        </Modal> */}
-                        {/* <Popup openPopup={openPopup}  setOpenPopup={setOpenPopup}/> 
-                         openPopup={openPopup}
-                        setOpenPopup={setOpenPopup} */}
-                        {/* </Popup> */}
+                        </Modal>
+                        {/* <Popup openPopup={openPopup}
+                        setOpenPopup={setOpenPopup}/> 
+                          */}
+                     
+
+                         {/* <Popup trigger={<button> 1</button>} position="right center" {...{overlayStyle}}>
+                          <div>Popup content here !!</div>
+                         </Popup> */}
 
                         
                         
@@ -71,7 +108,7 @@ const Table =() => {
               <td>
                 {/* Graphic Designer */}
                 {/* <small class="d-block">25</small> */}
-                <a href="#">123456789</a>
+                {/* <a href="#">123456789</a> */}
               </td>
               
             </tr>
